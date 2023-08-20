10d3cbb6b7fa0c47af3149520b083df7b3d81011
Vaibhav Shelar <vaibhavshelar269@gmail.com>
2023-08-20 20:05:34 +0530

    functionality is encapsulated into a separate get_metrics function

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    cpu_metric, mem_metric, message = get_metrics()
+    return render_template("index.html", cpu_metric=cpu_metric, mem_metric=mem_metric, message=message)
+
+def get_metrics():
     cpu_metric = psutil.cpu_percent()
     mem_metric = psutil.virtual_memory().percent
-    Message = None
-    if cpu_metric > 80 or mem_metric > 80:
-        Message = "High CPU or Memory Detected, scale up!!!"
-    return render_template("index.html", cpu_metric=cpu_metric, mem_metric=mem_metric, message=Message)
+    message = "High CPU or Memory Detected, scale up!!!" if cpu_metric > 80 or mem_metric > 80 else None
+    return cpu_metric, mem_metric, message
 
-if __name__=='__main__':
-    app.run(debug=True, host = '0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
