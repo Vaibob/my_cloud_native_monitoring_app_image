5f6f96111230f9af0f5d1e41c492a946060d36b9
Vaibhav Shelar <vaibhavshelar269@gmail.com>
2023-08-20 20:18:25 +0530

    Separate Deployment and Service Definitions added

--- eks.py
+++ eks.py
@@ -1,56 +1,44 @@
-#create deployment and service
 from kubernetes import client, config
 
-# Load Kubernetes configuration
-config.load_kube_config()
-
-# Create a Kubernetes API client
-api_client = client.ApiClient()
-
-# Define the deployment
-deployment = client.V1Deployment(
-    metadata=client.V1ObjectMeta(name="my-flask-app"),
-    spec=client.V1DeploymentSpec(
-        replicas=1,
-        selector=client.V1LabelSelector(
-            match_labels={"app": "my-flask-app"}
-        ),
-        template=client.V1PodTemplateSpec(
-            metadata=client.V1ObjectMeta(
-                labels={"app": "my-flask-app"}
-            ),
-            spec=client.V1PodSpec(
-                containers=[
-                    client.V1Container(
-                        name="my-flask-container",
-                        image="069684535409.dkr.ecr.us-east-1.amazonaws.com/my_cloud_native_monitoring_app_image",
-                        ports=[client.V1ContainerPort(container_port=5000)]
-                    )
-                ]
+def create_deployment(api_instance):
+    deployment = client.V1Deployment(
+        metadata=client.V1ObjectMeta(name="my-flask-app"),
+        spec=client.V1DeploymentSpec(
+            replicas=1,
+            selector=client.V1LabelSelector(match_labels={"app": "my-flask-app"}),
+            template=client.V1PodTemplateSpec(
+                metadata=client.V1ObjectMeta(labels={"app": "my-flask-app"}),
+                spec=client.V1PodSpec(
+                    containers=[
+                        client.V1Container(
+                            name="my-flask-container",
+                            image="069684535409.dkr.ecr.us-east-1.amazonaws.com/my_cloud_native_monitoring_app_image",
+                            ports=[client.V1ContainerPort(container_port=5000)]
+                        )
+                    ]
+                )
             )
         )
     )
-)
+    api_instance.create_namespaced_deployment(namespace="default", body=deployment)
 
-# Create the deployment
-api_instance = client.AppsV1Api(api_client)
-api_instance.create_namespaced_deployment(
-    namespace="default",
-    body=deployment
-)
-
-# Define the service
-service = client.V1Service(
-    metadata=client.V1ObjectMeta(name="my-flask-service"),
-    spec=client.V1ServiceSpec(
-        selector={"app": "my-flask-app"},
-        ports=[client.V1ServicePort(port=5000)]
+def create_service(api_instance):
+    service = client.V1Service(
+        metadata=client.V1ObjectMeta(name="my-flask-service"),
+        spec=client.V1ServiceSpec(
+            selector={"app": "my-flask-app"},
+            ports=[client.V1ServicePort(port=5000)]
+        )
     )
-)
+    api_instance.create_namespaced_service(namespace="default", body=service)
 
-# Create the service
-api_instance = client.CoreV1Api(api_client)
-api_instance.create_namespaced_service(
-    namespace="default",
-    body=service
-)
+def main():
+    config.load_kube_config()
+    api_client = client.ApiClient()
+    apps_v1_api = client.AppsV1Api(api_client)
+    core_v1_api = client.CoreV1Api(api_client)
+    create_deployment(apps_v1_api)
+    create_service(core_v1_api)
+
+if __name__ == "__main__":
+    main()
